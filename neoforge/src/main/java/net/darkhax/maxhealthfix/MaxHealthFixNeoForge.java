ae2ddb0b0861c13cc972331ef8f84b5c95806535
Tyler Hancock <darklime@live.ca>
2024-01-02 03:50:41 -0700

    Port to MC 1.20.2 and add support for NeoForge.

---
+++ neoforge/src/main/java/net/darkhax/maxhealthfix/MaxHealthFixNeoForge.java
@@ -0,0 +1,12 @@
+package net.darkhax.maxhealthfix;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class MaxHealthFixNeoForge {
+
+    public MaxHealthFixNeoForge() {
+
+        Constants.LOG.info("Loaded {} for Forge.", Constants.MOD_ID);
+    }
+}
