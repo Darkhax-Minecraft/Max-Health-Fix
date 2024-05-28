a5124a725eb2529459b9962c1fb88c0cd58cc0a2
Tyler Hancock <darklime@live.ca>
2024-05-28 04:27:15 -0600

    Remove unused imports.

--- common/src/main/java/net/darkhax/maxhealthfix/Constants.java
+++ common/src/main/java/net/darkhax/maxhealthfix/Constants.java
@@ -3,8 +3,6 @@ package net.darkhax.maxhealthfix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Random;
-
 public class Constants {
 
     public static final String MOD_ID = "maxhealthfix";
