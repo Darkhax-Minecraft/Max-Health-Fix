dcdcf0a7b06b9075d16a9111acf01648a4780e3d
Tyler Hancock <darklime@live.ca>
2024-10-05 23:31:34 -0600

    Fix parts of the mod not initializing on the client.

--- fabric/src/main/java/net/darkhax/maxhealthfix/fabric/impl/FabricMod.java
+++ fabric/src/main/java/net/darkhax/maxhealthfix/fabric/impl/FabricMod.java
@@ -1,12 +1,12 @@
 package net.darkhax.maxhealthfix.fabric.impl;
 
 import net.darkhax.maxhealthfix.common.impl.MaxHealthFixMod;
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 
-public class FabricModClient implements ClientModInitializer {
+public class FabricMod implements ModInitializer {
 
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         MaxHealthFixMod.getInstance().init();
     }
 }
